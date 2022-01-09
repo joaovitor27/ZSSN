89c8b969868820da845ac0cc9c20d4eb87f7f55a
joaovitor27 <joaovitor.monteiro.188@gmail.com>
2022-01-09 10:28:16 -0300

    Criação da view de listar e cadastrar itens!

--- apps/survivors/views.py
+++ apps/survivors/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.permissions import IsAdminUser
+
+from .models import Item, Survivor, Inventory
+from .serializers import ItemSerializer, SurvivorSerializer, InventorySerializer
 # Create your views here.
+
+
+class ItemListCreateAPIView(ListCreateAPIView):
+    
+    serializer_class = ItemSerializer
+    queryset = Item.objects.all()
+    permission_classes = [IsAdminUser]
