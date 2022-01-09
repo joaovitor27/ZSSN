3e386d36a09ab6ab4fe1603a6131d94599965253
joaovitor27 <joaovitor.monteiro.188@gmail.com>
2022-01-09 17:57:27 -0300

    View e url de atualizar o local do sobrevivente

--- apps/survivors/urls.py
+++ apps/survivors/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('item/', views.ItemListCreateAPIView.as_view(), name='items'),
     path('survivors/', views.SurvivorListCreateAPIView.as_view(), name='survivors'),
     path('inventories/', views.InventoryListCreateAPIView.as_view(), name='inventories'),
+    path('survivor-location-update/<int:survivor_id>/', views.SurvivorLocationUpdate.as_view(), name='survivor_location_update'),
 ]
--- apps/survivors/views.py
+++ apps/survivors/views.py
@@ -1,9 +1,11 @@
-from django.db.models.query import QuerySet
-from django.shortcuts import render
-from rest_framework import permissions
+from django.utils.translation import ugettext as _
+from django.shortcuts import get_object_or_404, render
 
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAdminUser
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
 
 from .models import Item, Survivor, Inventory
 from .serializers import ItemSerializer, SurvivorSerializer, InventorySerializer
@@ -29,4 +31,15 @@ class InventoryListCreateAPIView(ListCreateAPIView):
     serializer_class = InventorySerializer
     queryset = Inventory.objects.all()
     permissions_classes = [IsAdminUser]
+
+
+class SurvivorLocationUpdate(APIView):
+
+    def patch(self, request, survivor_id):
+        survivor = get_object_or_404(Survivor, pk=survivor_id)
+        serializer = SurvivorSerializer(survivor, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED, data=serializer.data)
+        return Response(status=status.HTTP_400_BAD_REQUEST, data={'message': _('Atualização não realizada.')})
 
