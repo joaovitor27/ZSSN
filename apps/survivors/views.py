efde1ea6669bf326825ab26a7b877ddda1e3b343
joaovitor27 <joaovitor.monteiro.188@gmail.com>
2022-01-10 10:05:34 -0300

    correção de erro na view

--- apps/survivors/views.py
+++ apps/survivors/views.py
@@ -1,5 +1,6 @@
 from django.utils.translation import ugettext as _
 from django.shortcuts import get_object_or_404, render
+from django.db.models import Sum
 
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAdminUser
